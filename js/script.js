4e1be65bdbdcf161a532fce77d98a3191191af25
joel-nwamba <joel.elyon.nwamba@gmail.com>
2025-04-14 20:50:41 -0400

    Add empty array to track information added

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const tabBtn = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
+
+// First we need to be able to input our information for it to display in an array of list 
+let leadTracker = [];
+
