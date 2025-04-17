ff5b161355e4a313db4ed305165e4861cb846619
joel-nwamba <joel.elyon.nwamba@gmail.com>
2025-04-16 20:23:06 -0400

    Able to input any information on the text field, but not yet displaying on the portal

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
+const saveBtn = document.getElementById("save-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const tabBtn = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
 
 // First we need to be able to input our information for it to display in an array of list 
+inputEl.value;
 let leadTracker = [];
-
+saveBtn.addEventListener("click", () => {
+    leadTracker.push(inputEl.value);
+    inputEl.value = "";
+})
